46733973376bdc7cb1a564e30c55027ebf7da78e
Sadiq-Memon <164412475+Sadiq-Memon@users.noreply.github.com>
2024-03-27 01:23:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    { name: "firstNumber", type: "number", message: "Enter Your Fist Number" },
+    { name: "SecondNumber", type: "number", message: "Enter Your Second Number" },
+    {
+        message: "Choose One Operator To Perform Tasks",
+        type: "list",
+        name: "Operators",
+        choices: ["Addition", "Substraction", "Multiplication", "Modulus"],
+    },
+]);
+if (answer.Operators === "Addition") {
+    console.log(answer.firstNumber + answer.SecondNumber);
+}
+else if (answer.Operators === "Substraction") {
+    console.log(answer.firstNumber - answer.SecondNumber);
+}
+else if (answer.Operators === "Multiplication") {
+    console.log(answer.firstNumber * answer.SecondNumber);
+}
+else if (answer.Operators === "Modulus") {
+    console.log(answer.firstNumber % answer.SecondNumber);
+}
+else {
+    console.log("Please Choose a Valid Operator");
+}
+;
